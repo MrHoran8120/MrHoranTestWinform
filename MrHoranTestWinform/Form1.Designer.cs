0fe90aec1762ab3e676069dfec3178966725fd81
MrHoran8120 <58581424+MrHoran8120@users.noreply.github.com>
2021-08-05 21:57:17 +1000

    random changes

--- MrHoranTestWinform/Form1.Designer.cs
+++ MrHoranTestWinform/Form1.Designer.cs
@@ -36,7 +36,7 @@ namespace MrHoranTestWinform
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 99.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(-80, 56);
+            this.label1.Location = new System.Drawing.Point(-78, 184);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(1013, 175);
             this.label1.TabIndex = 0;
